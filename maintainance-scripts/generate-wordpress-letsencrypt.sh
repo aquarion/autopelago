0e7c117acc31b2f0a975fff2f336b3d9a2ed667c
Nicholas Avenell <nicholas@istic.net>
2016-05-15 01:33:23 +0200

    NGinx Config & Letsencypt scripts

---
+++ maintainance-scripts/generate-wordpress-letsencrypt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo service nginx stop
+sudo letsencrypt certonly --standalone --domains \
+aquarionics.blogs.water.gkhs.net,www.aquarionics.com,aquarionics.com,\
+factionfiction.net,www.factionfiction.net,factionfiction.blogs.water.gkhs.net,\
+cleartextcontent.blogs.water.gkhs.net,www.cleartextcontent.co.uk,cleartextcontent.co.uk,\
+idlespeculation.blogs.water.gkhs.net,idlespeculation.foip.me,\
+omnyom.blogs.water.gkhs.net,omnyom.com,\
+herodiaries.blogs.water.gkhs.net,herodiaries.foip.me,\
+istic.blogs.water.gkhs.net,istic.net,\
+monthlymoon.blogs.water.gkhs.net,themonthlymoon.com,\
+blogs.water.gkhs.net
+sudo service nginx start
