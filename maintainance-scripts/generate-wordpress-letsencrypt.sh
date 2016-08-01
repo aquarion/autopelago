5f483b412d2c6c2d0d2b82394addade75dd1f9da
Nicholas Avenell <nicholas@istic.net>
2016-08-01 12:26:56 +0200

    Update certificates

--- maintainance-scripts/generate-wordpress-letsencrypt.sh
+++ maintainance-scripts/generate-wordpress-letsencrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo service nginx stop
-sudo letsencrypt certonly --standalone --domains \
+#sudo service nginx stop
+sudo certbot -v -t certonly --standalone --pre-hook "service nginx stop" --post-hook "service nginx start" --domains \
 istic.net,\
 archipelago.water.gkhs.net,\
 aquarionics.blogs.water.gkhs.net,www.aquarionics.com,aquarionics.com,old.aquarionics.com,\
@@ -8,9 +8,9 @@ wywo.blogs.water.gkhs.net,wywo.aquarionics.com,\
 factionfiction.net,www.factionfiction.net,factionfiction.blogs.water.gkhs.net,\
 cleartextcontent.blogs.water.gkhs.net,www.cleartextcontent.co.uk,cleartextcontent.co.uk,\
 idlespeculation.blogs.water.gkhs.net,idlespeculation.foip.me,\
-omnyom.blogs.water.gkhs.net,omnyom.com,www.omnyom.com\
+omnyom.blogs.water.gkhs.net,omnyom.com,www.omnyom.com,\
 herodiaries.blogs.water.gkhs.net,herodiaries.foip.me,\
-istic.blogs.water.gkhs.net,istic.co,istic.systems,istic.network\
+istic.blogs.water.gkhs.net,istic.co,istic.systems,istic.network,\
 monthlymoon.blogs.water.gkhs.net,themonthlymoon.com,\
 blogs.water.gkhs.net,\
 skute.me,alpha.skute.me,\
@@ -20,4 +20,4 @@ live.art.istic.net,imperial.istic.net,material.istic.net,\
 warehousebasement.com,www.warehousebasement.com,\
 dagon.church
 
-sudo service nginx start
+#sudo service nginx start
