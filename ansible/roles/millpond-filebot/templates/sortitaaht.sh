3e52da231ebe9606388be9552da17fe0c5aadb4a
Nicholas Avenell <nicholas@istic.net>
2017-01-02 17:09:49 +0100

    Update new location for media files

--- ansible/roles/millpond-filebot/templates/sortitaaht.sh
+++ ansible/roles/millpond-filebot/templates/sortitaaht.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 filebot -script fn:amc \
-        --output "/var/lib/plexmediaserver" \
+        --output "/var/lib/plexmediaserver/Media" \
         --log-file amc.log \
         --action hardlink \
         --conflict override -non-strict \
         --def music=y subtitles=en artwork=y \
         --def \
-                "seriesFormat=TV Shows/{n}/{'S'+s}/{s00e00} - {t}" \
+                "seriesFormat=TV/{n}/{'S'+s}/{s00e00} - {t}" \
                 "animeFormat=Anime/{n}/{fn}" \
-                "movieFormat=SpareDrive/Movies/{n} {y}/{fn}" \
+                "movieFormat=Movies/{n} {y}/{fn}" \
                 "musicFormat=Music/{n}/{fn}" \
         "/var/lib/plexmediaserver/Torrential/Completed"
