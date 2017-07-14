e51a7375764243010ac30b017d42850e6af4d482
Nicholas Avenell <nicholas@istic.net>
2017-07-15 00:34:45 +0200

    Add wiki.dagon.church to ansible & letsencrypt

--- maintainance-scripts/generate-archipelago-letsencrypt.sh
+++ maintainance-scripts/generate-archipelago-letsencrypt.sh
@@ -19,7 +19,7 @@ forums.profounddecisions.co.uk,\
 imperial.istic.net,altru.istic.net,log.istic.net,\
 live.art.istic.net,imperial.istic.net,material.istic.net,\
 warehousebasement.com,www.warehousebasement.com,\
-dagon.church,live.dagon.church,\
+dagon.church,live.dagon.church,wiki.dagon.church,\
 mechan.istic.net,\
 larp.me,api.larp.me,staging.larp.me,api.staging.larp.me,\
 diaspora.foip.me
