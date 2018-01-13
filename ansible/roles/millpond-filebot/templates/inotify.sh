9c3007091932dca384c90625a6534aee22ed8b3d
Nicholas Avenell <nicholas@istic.net>
2018-01-13 22:48:22 +0000

    Use notify instead of cron

---
+++ ansible/roles/millpond-filebot/templates/inotify.sh
@@ -0,0 +1,17 @@
+#!/bin/bash                                                                                                                                                                                                                                                                    
+
+HOME=/var/lib/plexmediaserver
+WATCH_PATH=$HOME/Torrential/Completed
+LOGFILE=$HOME/inotify_watch.log
+
+mkfifo $LOGFILE
+
+inotifywait -drq -o $LOGFILE -e create -e moved_to $WATCH_PATH
+
+trap "true" PIPE
+cat $LOGFILE | while read  path action file; 
+do
+	#echo "Saw $action in $path"; 
+	$HOME/bin/sortitaaht.sh; 
+done
+trap PIPE
