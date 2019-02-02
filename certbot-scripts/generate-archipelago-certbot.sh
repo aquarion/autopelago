df86dda66d214eb37760d97884f9bd6a5691dc9d
Nicholas Avenell <nicholas@istic.net>
2019-02-02 11:58:35 +0100

    carcosa wiki

--- certbot-scripts/generate-archipelago-certbot.sh
+++ certbot-scripts/generate-archipelago-certbot.sh
@@ -28,6 +28,6 @@ bless.ojaqs.foip.me,ojaqs.foip.me,\
 www.iglooteas.com,iglooteas.blogs.water.gkhs.net,\
 orion.nicholasavenell.com,\
 optim.istic.net,\
-carcosadreams.co.uk,www.carcosadreams.co.uk,alba.carcosadreams.co.uk,t67.carcosadreams.co.uk,forsaken.carcosadreams.co.uk,\
-carcosadreams.com,www.carcosadreams.com,alba.carcosadreams.com,t67.carcosadreams.com,forsaken.carcosadreams.com
+carcosadreams.co.uk,www.carcosadreams.co.uk,alba.carcosadreams.co.uk,t67.carcosadreams.co.uk,forsaken.carcosadreams.co.uk,wiki.carcosadreams.co.uk,\
+carcosadreams.com,www.carcosadreams.com,alba.carcosadreams.com,t67.carcosadreams.com,forsaken.carcosadreams.com,wiki.carcosadreams.com
 #sudo service nginx start
