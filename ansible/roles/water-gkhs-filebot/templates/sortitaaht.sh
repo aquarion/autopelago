43fd2b6be2767b24dee39f4a9d2d6d022815b31b
Nicholas Avenell <nicholas@istic.net>
2019-03-25 10:32:27 +0100

    Moving plex to archipelago

--- ansible/roles/water-gkhs-filebot/templates/sortitaaht.sh
+++ ansible/roles/water-gkhs-filebot/templates/sortitaaht.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export JAVA_OPTS=-Xmx1024m
 filebot -script fn:amc \
-        --output "{{ plex_home }}/Media" \
+        --output "{{ media_home }}" \
         --log-file amc.log \
         --action hardlink \
         --conflict override -non-strict \
@@ -11,4 +11,4 @@ filebot -script fn:amc \
                 "animeFormat=Anime/{n}/{fn}" \
                 "movieFormat=Movies/{n} {y}/{fn}" \
                 "musicFormat=Music/{n}/{fn}" \
-        "{{ plex_home }}/Torrential/Completed"
+        "{{ transmission_complete }}"
