ffd5f522979713ba4f6d69419dd69d54b7a7d854
Nicholas Avenell <nicholas@istic.net>
2019-09-27 00:04:51 +0100

    Add file locking to media scripts

--- ansible/roles/water-gkhs-filebot/templates/sortitaaht.sh
+++ ansible/roles/water-gkhs-filebot/templates/sortitaaht.sh
@@ -1,4 +1,38 @@
 #!/bin/sh
+# SPDX-License-Identifier: MIT
+ 
+## Copyright (C) 2009 Przemyslaw Pawelczyk <przemoc@gmail.com>
+##
+## This script is licensed under the terms of the MIT license.
+## https://opensource.org/licenses/MIT
+#
+# Lockable script boilerplate
+ 
+### HEADER ###
+ 
+LOCKFILE="/var/lock/`basename $0`"
+LOCKFD=99
+ 
+# PRIVATE
+_lock()             { flock -$1 $LOCKFD; }
+_no_more_locking()  { _lock u; _lock xn && rm -f $LOCKFILE; }
+_prepare_locking()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _no_more_locking EXIT; }
+ 
+# ON START
+_prepare_locking
+ 
+# PUBLIC
+exlock_now()        { _lock xn; }  # obtain an exclusive lock immediately or fail
+exlock()            { _lock x; }   # obtain an exclusive lock
+shlock()            { _lock s; }   # obtain a shared lock
+unlock()            { _lock u; }   # drop a lock
+ 
+### BEGIN OF SCRIPT ###
+ 
+# Simplest example is avoiding running multiple instances of script.
+exlock_now || exit 1
+ 
+
 export JAVA_OPTS=-Xmx1024m
 filebot -script fn:amc \
         --output "{{ media_home }}" \
--- ansible/roles/water-gkhs-filebot/templates/youtube_download.sh
+++ ansible/roles/water-gkhs-filebot/templates/youtube_download.sh
@@ -1,4 +1,44 @@
 #!/bin/bash
+# SPDX-License-Identifier: MIT
+ 
+## Copyright (C) 2009 Przemyslaw Pawelczyk <przemoc@gmail.com>
+##
+## This script is licensed under the terms of the MIT license.
+## https://opensource.org/licenses/MIT
+#
+# Lockable script boilerplate
+ 
+### HEADER ###
+ 
+LOCKFILE="/var/lock/`basename $0`"
+LOCKFD=99
+ 
+# PRIVATE
+_lock()             { flock -$1 $LOCKFD; }
+_no_more_locking()  { _lock u; _lock xn && rm -f $LOCKFILE; }
+_prepare_locking()  { eval "exec $LOCKFD>\"$LOCKFILE\""; trap _no_more_locking EXIT; }
+ 
+# ON START
+_prepare_locking
+ 
+# PUBLIC
+exlock_now()        { _lock xn; }  # obtain an exclusive lock immediately or fail
+exlock()            { _lock x; }   # obtain an exclusive lock
+shlock()            { _lock s; }   # obtain a shared lock
+unlock()            { _lock u; }   # drop a lock
+ 
+
+function dienow {
+	echo "Lock found $LOCKFILE"
+	exit 5
+}
+### BEGIN OF SCRIPT ###
+ 
+# Simplest example is avoiding running multiple instances of script.
+exlock_now || dienow
+ 
+# Remember! Lock file is removed when one of the scripts exits and it is
+#           the only script holding the lock or lock is not acquired at all.
 
 DIR={{ media_home }}/Youtube
 SCRATCH=$HOME/yt_scratch
