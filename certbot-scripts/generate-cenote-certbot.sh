5245ae4e2c568434e262f701b9530c92e84f4db8
Nicholas Avenell <nicholas@istic.net>
2020-05-06 00:17:47 +0000

    Add HTTPS for dhmstark.co.uk & comeoutandplay

--- certbot-scripts/generate-cenote-certbot.sh
+++ certbot-scripts/generate-cenote-certbot.sh
@@ -27,7 +27,9 @@ voicemail.aqxs.net,\
 www.deadbadgerdesigns.co.uk,deadbadgerdesigns.co.uk,\
 www.grantabruggehamstery.aqxs.net,www.grantabruggehamstery.co.uk,grantabruggehamstery.co.uk,\
 www.pinsandneedlescostume.co.uk,pinsandneedlescostume.co.uk,\
-thomwillis.uk,thomwillis.uk,moth.aqxs.net
+thomwillis.uk,thomwillis.uk,moth.aqxs.net,\
+comeoutandplay.aqxs.net,\
+dhmstark.co.uk,www.dhmstark.co.uk
 
 sudo certbot -t certonly --expand --standalone --pre-hook "service apache2 stop" --post-hook "service apache2 start" --domains nicholasavenell.com,www.nicholasavenell.com
  #mothinabutterfly.net,www.mothinabutterfly.net,\
