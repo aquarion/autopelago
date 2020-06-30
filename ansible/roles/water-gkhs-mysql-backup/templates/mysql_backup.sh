6e493843aba35c7f0bf68f8d8aec3b7ebe122a78
Nicholas Avenell <nicholas@istic.net>
2020-06-30 13:22:47 +0000

    Add exit on failure to mysql backup

--- ansible/roles/water-gkhs-mysql-backup/templates/mysql_backup.sh
+++ ansible/roles/water-gkhs-mysql-backup/templates/mysql_backup.sh
@@ -2,6 +2,24 @@
 #PASSWORD=$1 # Now using .my.cnf for password
 LOC={{mysql_backup_location}}/$1
 
+# Exit immediately if a pipeline returns non-zero.
+# Short form: set -e
+set -o errexit
+
+# Print a helpful message if a pipeline with non-zero exit code causes the
+# script to exit as described above.
+trap 'echo "Aborting due to errexit on line $LINENO. Exit code: $?" >&2' ERR
+
+# Allow the above trap be inherited by all functions in the script.
+# Short form: set -E
+set -o errtrace
+
+# Return value of a pipeline is the value of the last (rightmost) command to
+# exit with a non-zero status, or zero if all commands in the pipeline exit
+# successfully.
+set -o pipefail
+
+
 mkdir -p $LOC
 
 mysql -u root -e 'show databases;' --skip-column-names --skip-pager | while read DATABASE; 
