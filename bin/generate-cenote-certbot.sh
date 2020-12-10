53faa6ae30bc7bcd1855de7e4b09276f846fe7b5
Nicholas Avenell <nicholas@istic.net>
2020-12-10 16:39:11 +0000

    update certificate to remove afphrid

--- bin/generate-cenote-certbot.sh
+++ bin/generate-cenote-certbot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo certbot -t certonly --expand --standalone --pre-hook "service apache2 stop" --post-hook "service apache2 start" --domains \
+sudo certbot -n -t certonly --expand --standalone --cert-name cenote.gkhs.net --pre-hook "service apache2 stop" --post-hook "service apache2 start" --domains \
 cenote.gkhs.net,cenote.water.gkhs.net,ssl.aqxs.net,\
 advent.aqxs.net,advent-knowledge.info,www.advent-knowledge.info,\
 ashires.aqxs.net,bluebottle.aqxs.net,chesswiki.aqxs.net,dgold.aqxs.net,entimix.aqxs.net,locksmith.aqxs.net,makai.aqxs.net,martindenton.aqxs.net,rho21.aqxs.net,rjw76.aqxs.net,salavant.aqxs.net,taxellor.aqxs.net,tdk27.aqxs.net,\
@@ -17,7 +17,6 @@ www.gkhs.net,\
 aqxs.net,www.aqxs.net,\
 live.dailyphoto.aquarionics.com,\
 empireproxy.aqxs.net,fourrivers.foip.me,larp.me.uk,www.larp.me.uk,ornithocracy.aqxs.net,forgotten.foip.me,\
-quotes.afphrid.org.uk,\
 maelfroth.org,lampstand.aqxs.net,lampstand.maelfroth.org,obviouslyfakeurl.foip.me,rickroll.maelfroth.org,wiki.maelfroth.org,www.maelfroth.org,\
 lottery.aqxs.net,snakesandladders.aqxs.net,spycattes.aqxs.net,unhelpfulclue.aqxs.net,\
 ludo.istic.net,\
@@ -31,7 +30,9 @@ thomwillis.uk,thomwillis.uk,moth.aqxs.net,\
 comeoutandplay.aqxs.net,comeoutandplay.games,\
 dhmstark.co.uk,www.dhmstark.co.uk,kastark.co.uk,www.kastark.co.uk
 
-sudo certbot -t certonly --expand --standalone --pre-hook "service apache2 stop" --post-hook "service apache2 start" --domains nicholasavenell.com,www.nicholasavenell.com
+sudo certbot certonly -n --expand --dns-route53 -d *.aqxs.net && sudo service apache2 restart
+
+sudo certbot -n -t certonly --expand --standalone --pre-hook "service apache2 stop" --post-hook "service apache2 start" --domains nicholasavenell.com,www.nicholasavenell.com
  #mothinabutterfly.net,www.mothinabutterfly.net,\
  #swatt.treasuretrap.co.uk,
 #dailyphoto.aquarionics.com,
