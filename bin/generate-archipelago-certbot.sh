6c049c52828ada2fddc9c1a8d68415a521c5a895
Nicholas Avenell <nicholas@istic.net>
2021-08-02 12:06:11 +0000

    👁️‍🗨️ CONFIGURE THE PANOPTICON

--- bin/generate-archipelago-certbot.sh
+++ bin/generate-archipelago-certbot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo certbot -n -t certonly --expand --nginx --domains \
+sudo certbot -n -t certonly --expand --nginx --cert-name archipelago.water.gkhs.net --domains \
  archipelago.water.gkhs.net,\
 istic.net,\
-stream.aquarionics.com,aquarionics.blogs.water.gkhs.net,www.aquarionics.com,aquarionics.com,old.aquarionics.com,plex.aquarionics.com,vis.aquarionics.com,\
+stream.aquarionics.com,aquarionics.blogs.water.gkhs.net,www.aquarionics.com,aquarionics.com,old.aquarionics.com,plex.aquarionics.com,vis.aquarionics.com,panopticon.aquarionics.com,\
 wywo.blogs.water.gkhs.net,wywo.aquarionics.com,\
 factionfiction.net,www.factionfiction.net,factionfiction.blogs.water.gkhs.net,\
 cleartextcontent.blogs.water.gkhs.net,www.cleartextcontent.co.uk,cleartextcontent.co.uk,\
@@ -21,7 +21,7 @@ live.art.istic.net,imperial.istic.net,material.istic.net,\
 warehousebasement.com,www.warehousebasement.com,\
 dagon.church,live.dagon.church,wiki.dagon.church,\
 mechan.istic.net,\
-www.iglooteas.com,iglooteas.com,\
+www.iglooteas.com,\
 larp.me,api.larp.me,staging.larp.me,api.staging.larp.me,locations.larp.me,www.larp.me,\
 optim.istic.net,\
 www.larpfic.com,larpfic.com,www.lrpfic.com,lrpfic.com,\
@@ -30,17 +30,17 @@ forums.profounddecisions.co.uk
 # www.iglooteas.com,iglooteas.blogs.water.gkhs.net,\
 # omnyom.blogs.water.gkhs.net,omnyom.com,www.omnyom.com,\
 
-sudo certbot -n -t certonly --expand --nginx --domains nicholasavenell.com,www.nicholasavenell.com
+sudo certbot -n -t certonly --expand --nginx --cert-name nicholasavenell.com --domains nicholasavenell.com,www.nicholasavenell.com
 
 ## Istic wildcards
 
-AWS_PROFILE=istic-r53 sudo -E certbot -n certonly --expand --dns-route53 -d *.istic.net
-AWS_PROFILE=istic-r53 sudo -E certbot -n certonly --expand --dns-route53 -d *.carcosadreams.com -d *.carcosadreams.co.uk
+AWS_PROFILE=istic-r53 sudo -E certbot -n certonly --expand --cert-name istic.net --dns-route53 -d *.istic.net
+AWS_PROFILE=istic-r53 sudo -E certbot -n certonly --expand --cert-name carcosadreams.com --dns-route53 -d *.carcosadreams.com -d *.carcosadreams.co.uk
 
 ## AqCom Wildcards
 
-sudo certbot certonly -n --expand --dns-route53 -d *.foip.me 
-sudo certbot certonly -n --expand --dns-route53 -d hubris.house -d *.hubris.house 
+sudo certbot certonly -n --expand --dns-route53 --cert-name foip.me -d *.foip.me 
+sudo certbot certonly -n --expand --dns-route53 --cert-name hubris.house -d hubris.house -d *.hubris.house 
 
 sudo chown -R root:certbot_access /etc/letsencrypt
 sudo chmod g+rx /etc/letsencrypt
