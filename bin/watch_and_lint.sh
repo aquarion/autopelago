43b4d6d91f0956a2b819ea6493f2a58ce993d951
Nicholas Avenell <nicholas@istic.net>
2023-01-01 22:14:57 +0000

    Fix linting errors

---
+++ bin/watch_and_lint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+eval "$(direnv export bash)"
+
+TARGET=roles
+
+clear
+ansible-lint
+
+inotifywait -m -r -e modify -e create $TARGET --format="%f" \
+        | while read FILENAME
+                do
+                        clear
+                        echo Detected $FILENAME
+                        ansible-lint --write
+                        # mv "$TARGET/$FILENAME" "$PROCESSED/$FILENAME"
+                        # gzip "$PROCESSED/$FILENAME"
+                done
+                
