d4771819d69201e86d35e8b1af7f38b3266282f5
Nicholas Avenell <nicholas@istic.net>
2023-09-03 22:00:30 +0000

    Update cenote cert

--- bin/generate-cenote-certbot.sh
+++ bin/generate-cenote-certbot.sh
@@ -2,13 +2,12 @@
 
 sudo certbot -n -t certonly --expand --apache --cert-name cenote.gkhs.net --domains \
 cenote.gkhs.net,cenote.water.gkhs.net,\
-comic.mc.aqxs.net,comicpress.socksandpuppets.com,comic.socksandpuppets.com,ahdok.aqxs.net,socksandpuppets.com,www.socksandpuppets.com,\
+comicpress.socksandpuppets.com,comic.socksandpuppets.com,ahdok.aqxs.net,socksandpuppets.com,www.socksandpuppets.com,\
 treasuretrap.co.uk,nfnc.treasuretrap.co.uk,www.treasuretrap.co.uk,\
 emptytables.org,www.emptytables.org,\
 nanocountdown.com,www.nanocountdown.com,\
 avenell.me.uk,www.avenell.me.uk,avenell.me,www.avenell.me,\
 www.gkhs.net,\
-live.dailyphoto.aquarionics.com,\
 fourrivers.foip.me,larp.me.uk,www.larp.me.uk,ornithocracy.aqxs.net,forgotten.foip.me,\
 maelfroth.org,lampstand.maelfroth.org,forgotten.foip.me,obviouslyfakeurl.foip.me,rickroll.maelfroth.org,wiki.maelfroth.org,www.maelfroth.org,\
 ludo.istic.net,\
