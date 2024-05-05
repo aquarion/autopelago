1286823f827e36c211dba9ff825cc8681942aba5
Nicholas Avenell <nicholas@istic.net>
2024-05-05 18:54:27 +0100

    ➖ Remove Caisson

--- bin/generate-millpond-certbot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#sudo service nginx stop
-sudo letsencrypt -v -t --expand --apache --domains \
-treacle.mine.nu,\
-kettle.treacle.mine.nu,\
-media.treacle.mine.nu
-
-#sudo service nginx start
