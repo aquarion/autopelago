e5ef70ee4f2f82c4c6596b97e54ba7d6848a6524
Nicholas Avenell <nicholas@istic.net>
2025-01-04 21:22:24 +0000

    🛠 Fix some filebot config issues

--- roles/water-gkhs-filebot/templates/sortitaaht.sh
+++ roles/water-gkhs-filebot/templates/sortitaaht.sh
@@ -23,7 +23,7 @@ filebot -script fn:amc \
         --def clean=y \
         --action hardlink \
         --conflict override -non-strict \
-        --def music=y subtitles=en artwork=y extras=y \
+        --def music=y subtitles=en artwork=y \
         --def \
                 "seriesFormat=TV/{n}/{'S'+s}/{s00e00} - {t}" \
                 "animeFormat=Anime/{n}/{fn}" \
