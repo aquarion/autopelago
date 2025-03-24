b8de430cb84bcd8f83417dbfb658c0631e031012
Nicholas Avenell <nicholas@istic.net>
2025-03-24 13:45:08 +0000

    Add timestamp logging to certbot update script for better tracking

--- bin/update-all-certbots.sh
+++ bin/update-all-certbots.sh
@@ -6,9 +6,9 @@ ssh-add
 for host in firth cenote atoll;
   do
     echo "Updating autopelago on $host.istic.systems";
-    ssh -A $host.istic.systems cd code/autopelago/bin/ && git pull;
+    ssh -A $host.istic.systems "cd code/autopelago/bin/ && git pull" | ts "[$host:%T]" ;
     echo "Updating certbot on $host.istic.systems";
-    ssh $host.istic.systems code/autopelago/bin/generate-$host-certbot.sh &
+    ssh $host.istic.systems code/autopelago/bin/generate-$host-certbot.sh | ts "[$host:%T]" &
 done
 
 wait
