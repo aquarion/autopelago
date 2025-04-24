078e8d67e0a1f8469a77e340438b4b74e2b6fcc4
Nicholas Avenell <nicholas.avenell@iceye.com>
2025-04-24 21:57:49 +0100

    Update clear_bullshit to clear more bullshit

--- roles/water-gkhs-filebot/templates/clear_bullshit.sh
+++ roles/water-gkhs-filebot/templates/clear_bullshit.sh
@@ -3,8 +3,10 @@
 
 # Remove stray files from downloaded torrents
 
+find {{ transmission_complete }}/completed -name .xattr -type d -exec rm -rf {} \;
 find {{ transmission_complete }}/completed -links +1 -type f -delete -print
 find {{ transmission_complete }}/completed -iname screen\*.jpg -type f -delete -print
+find {{ transmission_complete }}/completed -iname screen\*.png -type f -delete -print
 find {{ transmission_complete }}/completed -name \*\.txt -delete -print
 find {{ transmission_complete }}/completed -name \*nfo -delete -print
 find {{ transmission_complete }}/completed -name \*sample\.??? -delete -print
@@ -13,3 +15,4 @@ find {{ transmission_complete }}/completed -name RARBG.txt -delete -print
 find {{ transmission_complete }}/completed -name RARBG_DO_NOT_MIRROR.exe -delete -print
 find {{ transmission_complete }}/completed -name \*tvm.jpg -delete -print
 find {{ transmission_complete }}/completed -type d -exec rmdir {} \;
+find {{ transmission_complete }}/completed -name www.YTS.MX.jpg -delete -print
