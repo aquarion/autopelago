faf71daef625a0412d0c6193ecb9ef36731ace66
Nicholas Avenell <nicholas.avenell@iceye.com>
2025-04-29 19:09:40 +0100

    Add Super Linter configuration and scripts
    - Create super-linter.local.env for local linter configuration.
    - Add super-linter.yml workflow for GitHub Actions integration.
    - Update .gitignore to exclude linter summary files.
    - Introduce superlinter.sh script to run the Super Linter in a Docker container.

---
+++ bin/superlinter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run \
+  -e DEFAULT_BRANCH=main \
+  -e LOG_LEVEL=DEBUG \
+  -e SAVE_SUPER_LINTER_SUMMARY=true   \
+  -e RUN_LOCAL=true   \
+  -v $PWD:/tmp/lint \
+  --env-file "$(git root)/.github/super-linter.local.env" \
+  ghcr.io/super-linter/super-linter:latest
