7847c4b9366d36faddadc4c11e9e0b4a6e6058c6
Nicholas Avenell <nicholas@istic.net>
2025-08-04 23:23:28 +0100

    Refactor upgrade-all-servers.sh to use dist-upgrade and handle reboot checks

--- bin/upgrade-all-servers.sh
+++ bin/upgrade-all-servers.sh
@@ -5,7 +5,15 @@ ssh-add
 
 for host in firth cenote atoll; do
 	echo "Updating $host"
-	ssh -A $host.istic.systems "sudo apt update -qq && sudo apt upgrade -y && sudo apt autoremove -y" | ts "[$host:%T]"
+	ssh -A $host.istic.systems "sudo apt-get update -qq && sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y" | ts "[$host:%T]"
+	ssh -A $host.istic.systems "sudo apt-get autoremove -y" | ts "[$host:%T]"
+
+	if ssh $host.istic.systems 'test -f /var/run/reboot-required'; then
+		echo "Reboot required on $host" | ts "[$host:%T]"
+		ssh -A $host.istic.systems "sudo reboot" | ts "[$host:%T]"
+	else
+		echo "No reboot required on $host" | ts "[$host:%T]"
+	fi
 done
 
 wait
