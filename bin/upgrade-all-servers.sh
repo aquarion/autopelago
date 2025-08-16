5b033744bb44190d196e9a273f94574f9c6d50e3
Nicholas Avenell <nicholas@istic.net>
2025-08-16 12:09:29 +0100

    Refactor upgrade-all-servers.sh to encapsulate host update logic in a function

--- bin/upgrade-all-servers.sh
+++ bin/upgrade-all-servers.sh
@@ -3,17 +3,22 @@
 eval "$(ssh-agent -s)"
 ssh-add
 
-for host in firth cenote atoll; do
-	echo "Updating $host"
-	ssh -A $host.istic.systems "sudo apt-get update -qq && sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y" | ts "[$host:%T]"
-	ssh -A $host.istic.systems "sudo apt-get autoremove -y" | ts "[$host:%T]"
-
-	if ssh $host.istic.systems 'test -f /var/run/reboot-required'; then
+function update_host {
+  local host=$1
+  echo "Updating $host"
+  ssh -A "$host.istic.systems" "sudo apt-get update -qq && sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y" | ts "[$host:%T]"
+  ssh -A "$host.istic.systems" "sudo apt-get autoremove -y" | ts "[$host:%T]"
+	
+	if ssh "$host.istic.systems" 'test -f /var/run/reboot-required'; then
 		echo "Reboot required on $host" | ts "[$host:%T]"
-		ssh -A $host.istic.systems "sudo reboot" | ts "[$host:%T]"
+		ssh -A "$host.istic.systems" "sudo reboot" | ts "[$host:%T]"
 	else
 		echo "No reboot required on $host" | ts "[$host:%T]"
 	fi
+}
+
+for host in firth cenote atoll; do
+	update_host "$host"&
 done
 
 wait
