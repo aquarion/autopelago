5a2507333d10441d10b130c57d9992399718e1a5
Nicholas Avenell <nicholas@istic.net>
2025-08-16 12:47:32 +0100

    Lint

--- bin/upgrade-all-servers.sh
+++ bin/upgrade-all-servers.sh
@@ -4,11 +4,11 @@ eval "$(ssh-agent -s)"
 ssh-add
 
 function update_host {
-  local host=$1
-  echo "Updating $host"
-  ssh -A "$host.istic.systems" "sudo apt-get update -qq && sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y" | ts "[$host:%T]"
-  ssh -A "$host.istic.systems" "sudo apt-get autoremove -y" | ts "[$host:%T]"
-	
+	local host=$1
+	echo "Updating $host"
+	ssh -A "$host.istic.systems" "sudo apt-get update -qq && sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y" | ts "[$host:%T]"
+	ssh -A "$host.istic.systems" "sudo apt-get autoremove -y" | ts "[$host:%T]"
+
 	if ssh "$host.istic.systems" 'test -f /var/run/reboot-required'; then
 		echo "Reboot required on $host" | ts "[$host:%T]"
 		ssh -A "$host.istic.systems" "sudo reboot" | ts "[$host:%T]"
@@ -18,7 +18,7 @@ function update_host {
 }
 
 for host in firth cenote atoll; do
-	update_host "$host"&
+	update_host "$host" &
 done
 
 wait
