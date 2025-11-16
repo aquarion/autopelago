23ea819a932ae4e74675f9a17cfc5741e3ea57cb
Nicholas Avenell <nicholas@istic.net>
2025-11-16 12:47:27 +0000

    Refactor remove_completed_torrents.sh to use JSON output from transmission-remote and improve torrent handling logic

--- roles/water-gkhs-filebot/templates/remove_completed_torrents.sh
+++ roles/water-gkhs-filebot/templates/remove_completed_torrents.sh
@@ -1,28 +1,76 @@
 #!/bin/bash
 # {{ ansible_managed }}
 
-# port, username, password
-#SERVER="localhost:9091 --auth user:pass"
+# Script to remove completed torrents from Transmission
+
+# Transmission server connection details are set in .netrc file in home directory
+
+if ! hash transmission-remote 2>/dev/null; then
+	echo "transmission-remote command not found. Please install Transmission CLI tools."
+	exit 1
+fi
+
+if ! hash jq 2>/dev/null; then
+	echo "jq command not found. Please install jq JSON processor."
+	exit 1
+fi
+
 
 # use transmission-remote to get torrent list from transmission-remote list
-TORRENTLIST=$(transmission-remote --list | sed -e '1d' -e '$d' | awk '{print $1}' | sed -e 's/[^0-9]*//g')
+TORRENTLIST=$(transmission-remote --json --list | jq -r '.arguments.torrents[].id')
+
+if [ -z "$TORRENTLIST" ]; then
+	echo "No torrents found in Transmission."
+	exit 0
+fi
+
+if [[ $TERM == "dumb" ]]; then # No color support
+	RED=""
+	GREEN=""
+	YELLOW=""
+	NC=""
+else
+	RED="\e[31m"
+	GREEN="\e[32m"
+	YELLOW="\e[33m"
+	NC="\e[0m" # No Color
+fi
+
+TXT_COMPLETE="${GREEN}Completed${NC}"
+TXT_INCOMPLETE="${YELLOW}Incomplete${NC}"
+TXT_AGEDOUT="${RED}Aged Out${NC}"
+
+CURRENT_EPOCH=$(date +%s)
+AGE_OUT_CUTOFF=$((2 * 30 * 24 * 60 * 60)) # 60 days in seconds
 
 # for each torrent in the list
-for TORRENTID in $TORRENTLIST; do
-	INFO=$(transmission-remote "$SERVER" --torrent "$TORRENTID" --info)
-	echo -e "Processing #$TORRENTID - $(echo "$INFO" | sed -e 's/.*Name: \(.*\) Hash.*/\1/')"
+transmission-remote --json --list | jq -r '.arguments.torrents[].id' | while read -r TORRENTID; do
+	INFO=$(transmission-remote --torrent "$TORRENTID" --json --info)
+	NAME=$(echo "$INFO" | jq -r .arguments.torrents[0].name)
+	FINISHED=$(echo "$INFO" | jq -r .arguments.torrents[0].isFinished)
+	RATIO=$(echo "$INFO" | jq -r .arguments.torrents[0].uploadRatio)
+	LAST_ACTIVITY=$(echo "$INFO" | jq -r .arguments.torrents[0].activityDate)
+	AGE=$((CURRENT_EPOCH - LAST_ACTIVITY))
 
-	# check if torrent download is completed
-	DL_COMPLETED=$(echo "$INFO" | grep "Done: 100%\|Done: 99\..%")
-	# check torrents current state is
-	#STATE_STOPPED=`echo $INFO | grep "State: Seeding\|State: Stopped\|State: Finished\|State: Idle"`
-	STATE_STOPPED=$(echo "$INFO" | grep "State: Finished")
+	if [[ $LAST_ACTIVITY == "null" ]]; then
+		LAST_ACTIVITY_WORDS="N/A"
+	else
+		LAST_ACTIVITY_WORDS=$(date -d @"$LAST_ACTIVITY" +"%Y-%m-%d %H:%M:%S")
+	fi
+	
+	echo -en "#$TORRENTID - $NAME - "
 
-	# if the torrent is "Stopped", "Finished", or "Idle after downloading 100%"
-	if [ "$DL_COMPLETED" ] && [ "$STATE_STOPPED" ]; then
-		echo "Torrent #$TORRENTID is completed. Removing torrent from list."
-		transmission-remote "$SERVER" --torrent "$TORRENTID" --remove
-	#else
-	#    echo "Torrent #$TORRENTID is not completed. It is`transmission-remote $SERVER --torrent $TORRENTID --info | grep "Done:"` Ignoring."
+	# Check if the torrent is finished
+	if [ "$FINISHED" = "true" ]; then
+		echo -e " is ${GREEN}${TXT_COMPLETE}${NC} Removing."
+		transmission-remote --torrent "$TORRENTID" --remove
+	else
+		if (( AGE > AGE_OUT_CUTOFF )); then
+			echo -e " is ${RED}${TXT_AGEDOUT}${NC}, Last activity $LAST_ACTIVITY_WORDS. Removing."
+			transmission-remote --torrent "$TORRENTID" --remove
+			continue
+		else
+			echo -e " is ${YELLOW}${TXT_INCOMPLETE}${NC}, Ratio is $RATIO. Ignoring."
+		fi
 	fi
 done
