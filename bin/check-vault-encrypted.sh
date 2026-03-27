bf1cea67d6fcba505dec1f8d5f3fbca5a8993865
Nicholas Avenell <nicholas@istic.net>
2026-03-27 20:01:56 +0000

    🧵 Results of pre-commit additions

--- bin/check-vault-encrypted.sh
+++ bin/check-vault-encrypted.sh
@@ -2,9 +2,9 @@
 # Verify that all passed files are encrypted with ansible-vault
 failed=0
 for file in "$@"; do
-  if ! head -1 "$file" | grep -q '^\$ANSIBLE_VAULT'; then
-    echo "ERROR: $file is not encrypted with ansible-vault"
-    failed=1
-  fi
+	if ! head -1 "$file" | grep -q '^\$ANSIBLE_VAULT'; then
+		echo "ERROR: $file is not encrypted with ansible-vault"
+		failed=1
+	fi
 done
 exit $failed
